423ede6f1fba2184e47dd74ef272337e7d0327ea
Katherin Vela <katherin.vela@hotmail.com>
2019-09-14 21:43:00 -0500

    js

--- scripts/curso.js
+++ scripts/curso.js
@@ -1,22 +1,11 @@
-// Inserta una clase a un elemento buscado
-function agregarInformacion(coleccion, comparar, insertar) {
-    if (coleccion != null) {
-        var i;
-        for (i = 0; i < coleccion.length; i++) {
-            recorrerPadres(coleccion[i], comparar).classList.add(insertar);
-        }
-    }
-}
-
 document.addEventListener("DOMContentLoaded", function() {
     console.log('Cargando js');
     let elementoComparar = document.getElementById('section-0');
-    if (elementoComparar.classList.contains('state-visible') === true) {
-        let elementoBuscar = document.getElementById('page-header');
-        agregarInformacion(elementoBuscar, elementoComparar, 'home-ruta');
+    if (elementoComparar.classList.contains('state-visible') == true) {
+        document.getElementById('page-header').classList.add('home-ruta');
     }
 
-    if (elementoComparar.classList.contains('home-ruta') === true) {
+    if (elementoComparar.classList.contains('home-ruta') == true) {
         let textoBienvenida = document.createElement('p');
         textoBienvenida.classList.add('texto-bienvenida');
         let titulo = document.getElementById('page-mast').firstElementChild;
