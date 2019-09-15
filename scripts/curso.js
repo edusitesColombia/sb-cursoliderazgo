6af0a230bd67509e1fd01bf14a517708e25d0981
Katherin Vela <katherin.vela@hotmail.com>
2019-09-14 22:27:01 -0500

    js cambios

--- scripts/curso.js
+++ scripts/curso.js
@@ -5,12 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     while (s0Visible == true) {
         let header = document.getElementById('page-header');
-        header.classList.toggle('home-ruta');
+        header.classList.add('home-ruta');
         let textoBienvenida = document.createElement('p');
         textoBienvenida.classList.add('texto-bienvenida');
         let titulo = document.getElementById('page-mast').firstElementChild;
         textoBienvenida.innerText = 'Esperamos que este espacio virtual, permita a todos los docentes acercarse a reflexiones y ejercicios que promuevan su capacidad de liderazgo en sus contextos escolares';
         titulo.insertAdjacentElement('afterend', textoBienvenida);
-        document.getElementsByClassName('chapter-title').onclick = header.classList.toggle('home-ruta');
+        document.getElementsByClassName('chapter-title').onclick = header.classList.remove('home-ruta');
     }
 });
