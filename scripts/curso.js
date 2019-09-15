1b594f51b3daa122f1839b9c2ee4951f77fcff1d
Katherin Vela <katherin.vela@hotmail.com>
2019-09-15 12:41:12 -0500

    js

--- scripts/curso.js
+++ scripts/curso.js
@@ -1,25 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("cuando carga");
-    window.onload = function() {
-        console.log("cuando cargo");
-        if (document.getElementById('section-0').classList.contains('state-visible') === true) {
-            console.log(document.getElementById('section-0').classList);
-            document.getElementById('moodle-page').classList.add('home-ruta');
-        }
+function insertaClaseRuta() {
+    if (document.getElementById('section-0').classList.contains('state-visible') === true) {
+        document.getElementById('moodle-page').classList.add('home-ruta');
+    }
+}
 
-        if (document.getElementById('moodle-page').classList.contains('home-ruta') === true) {
-            let textoBienvenida = document.createElement('p');
-            textoBienvenida.classList.add('texto-bienvenida');
-            let titulo = document.getElementById('page-mast').firstElementChild;
-            textoBienvenida.innerText = 'Esperamos que este espacio virtual, permita a todos los docentes acercarse a reflexiones y ejercicios que promuevan su capacidad de liderazgo en sus contextos escolares';
-            titulo.insertAdjacentElement('afterend', textoBienvenida);
-        }
-        let modulos = document.getElementsByClassName('chapter-title');
-        for (i = 0; i < modulos.length; i++) {
-            modulos[i].addEventListener('click', function() {
-                document.getElementById('moodle-page').classList.remove('home-ruta');
-                document.getElementById('moodle-page').classList.add('por-defecto');
-            });
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    insertaClaseRuta();
+    if (document.getElementById('moodle-page').classList.contains('home-ruta') === true) {
+        let textoBienvenida = document.createElement('p');
+        textoBienvenida.classList.add('texto-bienvenida');
+        let titulo = document.getElementById('page-mast').firstElementChild;
+        textoBienvenida.innerText = 'Esperamos que este espacio virtual, permita a todos los docentes acercarse a reflexiones y ejercicios que promuevan su capacidad de liderazgo en sus contextos escolares';
+        titulo.insertAdjacentElement('afterend', textoBienvenida);
+    }
+
+    let modulos = document.getElementsByClassName('chapter-title');
+    for (i = 0; i < modulos.length; i++) {
+        modulos[i].addEventListener('click', function() {
+            document.getElementById('moodle-page').classList.remove('home-ruta');
+            document.getElementById('moodle-page').classList.add('por-defecto');
+        });
+    }
+
+});
+document.addEventListener('hashchange', function() {
+    console.log("cuando cambio");
+    let control = document.getElementById('moodle-page');
+    if (control.classList.contains('por-defecto') === true) {
+        control.classList.remove('por-defecto');
+        insertaClaseRuta();
     }
 });
