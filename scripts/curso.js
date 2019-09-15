c29d87179aa54f864a365853011f2483eccec64f
Katherin Vela <katherin.vela@hotmail.com>
2019-09-14 20:19:46 -0500

    insertar clase en page-header para hacer ruta

---
+++ scripts/curso.js
@@ -0,0 +1,16 @@
+// Inserta una clase a un elemento buscado
+function agregarInformacion(coleccion, comparar, insertar) {
+    if (coleccion != null) {
+        var i;
+        for (i = 0; i < coleccion.length; i++) {
+            recorrerPadres(coleccion[i], comparar).classList.add(insertar);
+        }
+    }
+}
+document.addEventListener("DOMContentLoaded", function() {
+    let elementoComparar = document.getElementById(section - 0);
+    if (elementoComparar.classList.contains(state - visible) === true) {
+        let elementoBuscar = document.getElementsById(page - header);
+        agregarInformacion(elementoBuscar, elementoComparar, 'home-ruta');
+    }
+})
