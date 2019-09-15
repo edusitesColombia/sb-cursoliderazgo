f91d292090a569f5f10a443d83094ba2d9d502b9
Katherin Vela <katherin.vela@hotmail.com>
2019-09-15 11:36:31 -0500

    js

--- scripts/curso.js
+++ scripts/curso.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log("cuando carga");
+    document.getElementById('moodle-page').classList.remove('home-ruta');
+    document.getElementById('moodle-page').classList.remove('por-defecto');
+
     if (document.getElementById('section-0').classList.contains('state-visible') == true) {
         console.log(document.getElementById('section-0').classList.contains('state-visible'));
         document.getElementById('moodle-page').classList.add('home-ruta');
@@ -21,9 +24,4 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
-});
-document.addEventListener('hashchange', function() {
-    console.log("cuando cambia");
-    document.getElementById('moodle-page').classList.remove('home-ruta');
-    document.getElementById('moodle-page').classList.remove('por-defecto');
 });
