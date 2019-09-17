96bfc9f71aa8efdd873de9cdb6f24cd49bf46116
Katherin Vela <katherin.vela@hotmail.com>
2019-09-17 16:18:05 -0500

    js cambios

--- scripts/curso.js
+++ scripts/curso.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 window.addEventListener('hashchange', function() {
-    if (claseControl == true && identPrincipal.classList.contains('por-defecto') == true && location.hash == '') {
+    if (claseControl == true && identPrincipal.classList.contains('por-defecto') == true) {
         insertaClaseRuta();
         let modulos = document.getElementsByClassName('chapter-title');
         for (i = 0; i < modulos.length; i++) {
