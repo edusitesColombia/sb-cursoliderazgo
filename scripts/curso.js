9ef021733711f4e68c53de068306492beac0ded7
Katherin Vela <katherin.vela@hotmail.com>
2019-09-17 20:42:40 -0500

    js

--- scripts/curso.js
+++ scripts/curso.js
@@ -31,7 +31,7 @@ function marcarPadreClase(elemento, nombreClaseAgregar) {
     var i;
     for (i = 0; i < elemento.length; i++) {
         elementoEncontrado = recorrerPadres(elemento[i]);
-        elementoEncontrato.classList.add(nombreClaseAgregar);
+        elementoEncontrado.classList.add(nombreClaseAgregar);
     }
 }
 
