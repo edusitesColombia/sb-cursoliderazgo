49cf678d466bca95955c3e1dd874dbea336ef2f1
Katherin Vela <katherin.vela@hotmail.com>
2019-09-17 20:31:33 -0500

    ruta

--- scripts/curso.js
+++ scripts/curso.js
@@ -13,6 +13,29 @@ function insertaClasePorDefecto() {
     identPrincipal.classList.remove('home-ruta');
     identPrincipal.classList.add('por-defecto');
 }
+
+// Busca el padre con etiqueta <li> de un elemento y lo retorna.
+function recorrerPadres(elementoComparar) {
+    var padre = elementoComparar;
+    var tipoElemento = "";
+    while (tipoElemento != "LI") {
+        padre = padre.parentNode;
+        tipoElemento = padre.nodeName;
+    }
+    return padre;
+}
+
+// Inserta una clase al padre encontrado de cada elemento de la colección pasada como parametro.
+function marcarPadreClase(elemento, nombreClaseAgregar) {
+    var elementoEncontrado;
+    var i;
+    for (i = 0; i < elemento.length; i++) {
+        elementoEncontrado = recorrerPadres(elemento[i]);
+        document.getElementById(elementoEncontrado.id).classList.add(nombreClaseAgregar);
+    }
+}
+
+
 //el hash lo uso como forma de controlar la navegacion con los botones del navegador porque las clases estan cargando lento y tocaria hacer esto cuando este todo cargado.
 document.addEventListener('DOMContentLoaded', function() {
     if ((claseControl == true && section0.classList.contains('state-visible') == true && location.hash == '') || location.hash == '#section-0') {
@@ -29,6 +52,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 insertaClasePorDefecto();
             });
         }
+        //identifica si el modulo fue completado y agrega la clase
+        marcarPadreClase(document.getElementsByClassName('snap-section-complete'), 'modulo-completado');
+
     } else if (claseControl == true && location.hash != '' && location.hash != 'section-0') {
         insertaClasePorDefecto();
     }
