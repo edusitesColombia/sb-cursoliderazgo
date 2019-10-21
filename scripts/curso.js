6aa247b8d8a226a7c364a7c9fbec36466f232395
Katherin Vela <katherin.vela@hotmail.com>
2019-10-21 11:19:43 -0500

    script

--- scripts/curso.js
+++ scripts/curso.js
@@ -46,7 +46,7 @@ function marcarPadreClase(elemento, nombreClaseAgregar) {
 
 //el hash lo uso como forma de controlar la navegacion con los botones del navegador porque las clases estan cargando lento y tocaria hacer esto cuando este todo cargado.
 document.addEventListener('DOMContentLoaded', function() {
-    if (claseControl == true) {
+    if (claseControl == true && (document.body.classList.contains('path-user') == false)) {
         if (location.hash == '' || location.hash == '#section-0') {
             insertaClaseRuta();
             let textoBienvenida = document.createElement('p');
